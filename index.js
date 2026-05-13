e397eaa05e5d2ae5f1ff035a0b14f40bc3582e17
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-13 17:22:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (r % 7 == 0) {
     phi = 3.14;
 }
 // console.log(phi);
-const k = 2 * phi * r;
-const l = phi * r * r;
-console.log(k);
-console.log(l);
+const K = 2 * phi * r;
+const L = phi * r * r;
+console.log(K);
+console.log(L);
