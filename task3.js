44bd41f205cb0fd13fb23766c1fc0eda18b47d83
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-13 16:01:32 +0700

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,13 @@
+// Membuat Perulangan yang menghasilkan segitiga siku-siku
+
+let str = "";
+for (let j = 1; j <= 5; j++) {
+    str += "*"
+    console.log(str);
+}
+
+// ketika j = 1, str = "*"
+// ketika j = 2, str = "**"
+// ketika j = 3, str = "***"
+// ketika j = 4, str = "****"
+// ketika j = 5, str = "*****"
