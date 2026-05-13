392abf43aecd1f7f0f659162217c84ae7b72446d
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-13 17:21:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Membuat program hitung luas dan keliling lingkaran
 let r = 7;
-
+if (typeof r !== "number") {
+    console.log(error = "r must be a number");
+    return
+}
 if (r % 7 == 0) {
     phi = 22 / 7;
 } else {
     phi = 3.14;
 }
 // console.log(phi);
-const K = 2 * phi * r;
-const L = phi * r * r;
-console.log(K);
-console.log(L);
+const k = 2 * phi * r;
+const l = phi * r * r;
+console.log(k);
+console.log(l);
