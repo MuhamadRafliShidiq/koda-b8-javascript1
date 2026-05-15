3cadff1de030e7ff5faa61074a00801e090296b2
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-15 14:07:02 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 // Membuat program hitung luas dan keliling lingkaran
-let r = "abc";
-if (typeof r !== "number") {
-    console.log(error = "r must be a number");
-    return
+// let r = 7;
+// if (typeof r !== "number") {
+//     console.log(error = "r must be a number");
+//     return
+// }
+// if (r % 7 == 0) {
+//     phi = 22 / 7;
+// } else {
+//     phi = 3.14;
+// }
+// // console.log(phi);
+// const k = 2 * phi * r;
+// const l = phi * r * r;
+// console.log(k);
+// console.log(l);
+
+// Mengganti dengan menggunakan function
+function lingkaran(r) {
+    if (typeof r !== "number") {
+        console.log(error = "r must be a number");
+        return
+    }
+    if (r % 7 == 0) {
+        phi = 22 / 7;
+    } else {
+        phi = 3.14;
+    }
+    const k = 2 * phi * r;
+    const l = phi * r * r;
+    return [k, l];
 }
-if (r % 7 == 0) {
-    phi = 22 / 7;
-} else {
-    phi = 3.14;
-}
-// console.log(phi);
-const K = 2 * phi * r;
-const L = phi * r * r;
-console.log(K);
-console.log(L);
+
+console.log(lingkaran(7));
